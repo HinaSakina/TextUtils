89e108d5fdd79fba05530867904992c1dc0da238
HinaSakina <hinaSakina4@gmail.com>
2023-08-05 18:28:45 +0500

    textutils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+// import { Router } from 'react-router-dom';
 import './App.css';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import About from './components/About';
+import React, { useState } from 'react';
+
+
+
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000)
+  }
+
+  const toggleMode = () => {
+    if (mode == 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enable", "success")
+      document.title = "textutles-dark mode"
+      // setInterval(()=>{
+      //   document.title=" install textutles"
+
+      // },2000)
+      // setInterval(()=>{
+      //   document.title="textutles is amazing"
+
+      // },1500)
+      // document.body.style.color ='white';    // by using this we can change text color
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white ';
+      showAlert("Light mode has been enable", "success")
+      document.title = "textutles-light mode"
+
+      // document.body.style.color ='black';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Navbar tittle="Textutiles" aboutText="About" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className='container my-3'>
+
+              <TextForm showAlert={showAlert} heading="Enter Text" mode={mode} />
+        </div>
+
+    </>
+
+
   );
 }
 
